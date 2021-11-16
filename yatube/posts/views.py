c64e906d36924a0f5b7f48ba211af736fd3ccf97
Даниил Цыганов <daniil.tsiganov@gmail.com>
2021-11-16 23:09:56 +0300

    Уже почти сайт.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,17 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+#Главная страница
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+
+#Страница сообществ
+def group_posts(request, slug):
+    return HttpResponse(f'Сообщество {slug}')
+
+
